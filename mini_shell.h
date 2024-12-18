109334dee6a80eb1a6a983bf3416f613965e3f93
novailable <97833342+novailable@users.noreply.github.com>
2024-12-18 10:23:11 +0000

    added makefile, .h and Libft

--- mini_shell.h
+++ mini_shell.h
@@ -1,10 +1,16 @@
 #ifndef MINI_SHELL_H
 #define MINI_SHELL_H
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <signal.h>
+#include <sys/stat.h>
+#include <dirent.h>
+
+// char	**ft_split(char *str, char *delimiter, char	*ignchar);
 
 #endif
