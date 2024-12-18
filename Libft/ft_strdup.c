fbea2079f32a282acf659555c9872eeaf63f291d
novailable <97833342+novailable@users.noreply.github.com>
2024-12-18 10:21:47 +0000

    added Libft and main

---
+++ Libft/ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoo <marvin@42.fr>                         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/22 21:13:38 by aoo               #+#    #+#             */
+/*   Updated: 2024/05/22 21:13:46 by aoo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char	*result;
+	int		i;
+
+	i = 0;
+	result = (char *)malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (result == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		result[i] = s[i];
+		i++;
+	}
+	result[i] = 0;
+	return (result);
+}
