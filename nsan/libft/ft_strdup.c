4e9cdcd2dc04e5467a90851b6d686466bed0695a
novailable <97833342+novailable@users.noreply.github.com>
2024-12-18 09:21:16 +0000

    rename folder

---
+++ nsan/libft/ft_strdup.c
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nsan <nsan@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/01 17:53:14 by nsan              #+#    #+#             */
+/*   Updated: 2024/06/01 17:53:15 by nsan             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *src)
+{
+	char	*dest;
+	int		i;
+
+	i = 0;
+	while (src[i] != '\0')
+		i++;
+	dest = (char *)malloc(sizeof(char) * (i + 1));
+	if (dest == NULL)
+		return (NULL);
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/*int	main(void)
+{
+	char	*str;
+	char	*newstr;
+
+	str = "Hello world";
+	newstr = ft_strdup(str);
+	printf("%s", newstr);
+}*/
