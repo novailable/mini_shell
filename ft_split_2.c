d98273ba52e88a8c0da6305af69ae19d29acfa03
novailable <97833342+novailable@users.noreply.github.com>
2024-12-19 20:17:36 +0000

    added

--- ft_split_2.c
+++ ft_split_2.c
@@ -1,19 +1,182 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split_2.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoo <aoo@student.42singapore.sg>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/19 13:35:49 by aoo               #+#    #+#             */
+/*   Updated: 2024/12/19 20:16:21 by aoo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-int	word_count(char *str, char *delimiters, char *ignchars)
+#include "mini_shell.h"
+
+int	is_quote(char *q_chars, char c, int *in_quote)
+{
+	if (ft_strchr(q_chars, c))
+	{
+		if (!*in_quote)
+			*in_quote = c;
+		else if (*in_quote == c)
+			*in_quote = !*in_quote;
+	}
+	return (*in_quote);
+}
+
+// int	word_count(char *str, char *d_chars, char *q_chars)
+// {
+// 	int	count;
+// 	int	in_word;
+// 	int	in_quote;
+
+// 	in_word = 0;
+// 	in_quote = 0;
+// 	count = 0;
+// 	while (*str)
+// 	{
+// 		if (is_quote(q_chars, *str, &in_quote))
+// 		{
+// 			str++;
+// 			str = ft_strchr(str, in_quote) + 1;
+// 			count++;
+// 			in_quote = 0;
+// 		}
+// 		if (!ft_strchr(d_chars, *str) && !in_word && !in_quote)
+// 		{
+// 			in_word = 1;
+// 			count++;
+// 		}
+// 		else if (ft_strchr(d_chars, *str) || in_quote)
+// 			in_word = 0;
+// 		str++;
+// 		if(in_quote && !*str)
+// 			return (0);
+// 		// is_quote(q_chars, *str, &in_quote);
+// 		// if (!ft_strchr(d_chars, *str) && !in_word && !in_quote)
+// 		// {
+// 		// 	in_word = 1;
+// 		// 	count++;
+// 		// }
+// 		// else if (ft_strchr(d_chars, *str) || in_quote)
+// 		// 	in_word = 0;
+// 		// str++;
+// 		// if (in_quote && !*str)
+// 		// 	return (0);
+// 	}
+// 	return (count);
+// }
+
+int	word_count(char *str, char *d_chars, char *q_chars)
 {
 	int	count;
+	int	in_word;
+	int	in_quote;
 
+	in_word = 0;
+	in_quote = 0;
 	count = 0;
 	while (*str)
 	{
-		
+		if (is_quote(q_chars, *str, &in_quote))
+		{
+			str++;
+			str = ft_strchr(str, in_quote) + 1;
+			in_quote = 0;
+			count++;
+		}
+		if (!ft_strchr(d_chars, *str))
+		{
+			count++;
+			while (*str && !ft_strchr(d_chars, *str) && !ft_strchr(q_chars, *str))
+				str++;
+		}
+		str++;
 	}
+	return (count);
 }
 
-
-char	**ft_split(char *str, char *delimiters, char *ignchars)
+static int	ft_strndup(char **r, const char *src, int len, int i)
 {
-	char	**result;
+	char	*result;
+	int		j;
 
+	j = 0;
+	result = (char *)malloc((len + 1) * sizeof(char));
+	if (!result)
+	{
+		while (i >= 0)
+			free(r[i--]);
+		free(r);
+		return (0);
+	}
+	while (j < len)
+	{
+		result[j] = src[j];
+		j++;
+	}
+	result[j] = 0;
+	r[i] = result;
+	return (1);
+}
 
+char	*make_splitting(char *str, char *d_chars, char *q_chars, char **result)
+{
+	char	*start;
+	int		in_quote;
+	int		i;
+
+	i = 0;
+	while (result[i])
+		i++;
+	in_quote = 0;
+	if (is_quote(q_chars, *str, &in_quote))
+	{
+		start = ++str;
+		str = ft_strchr(str, in_quote);
+		ft_strndup(result, start, str - start, i++);
+		in_quote = 0;
+		str++;
+	}
+	else if (!ft_strchr(d_chars, *str))
+	{
+		start = str;
+		while (*str && !ft_strchr(d_chars, *str) && !ft_strchr(q_chars, *str))
+			str++;
+		ft_strndup(result, start, str - start, i++);
+	}
+	else
+		str++;
+	return (str);
+}
+
+char	**ft_split_2(char *str, char *d_chars, char *q_chars)
+{
+	char		**result;
+	int			w_count;
+	int			i;
+	if (!str)
+		return (NULL);
+	w_count = word_count(str, d_chars, q_chars);
+	if (w_count == 0)
+		return (NULL);	
+	result = (char **)malloc((w_count + 1) * sizeof(char *));
+	if (!result)
+		return (perror("malloc failed"), NULL);
+	i = 0;
+	while (i < w_count)
+		result[i++] = NULL;
+	while (*str)
+	{
+		str = make_splitting(str, d_chars, q_chars, result);
+		if (!str)
+		{
+			while (i-- > 0)
+				free(result[i]);
+			free(result);
+			return (NULL);
+		}
+	}
+	result[w_count] = NULL;
+	return (result);
 }
