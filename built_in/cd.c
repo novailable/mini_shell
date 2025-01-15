b4b3a280585db2686a577d8c95c201f37618c053
novailable <97833342+novailable@users.noreply.github.com>
2025-01-15 09:58:58 +0000

    totally structured

---
+++ built_in/cd.c
@@ -0,0 +1,1 @@
+//cd built-ins
---
+++ built_in/unset.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   unset.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aoo <aoo@student.42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/06 18:15:06 by aoo               #+#    #+#             */
+/*   Updated: 2025/01/14 01:07:51 by aoo              ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../mini_shell.h"
+
+void	unset(char **args, t_list **envp)
+{
+	t_list	*prev_node;
+	t_list	*curr_node;
+
+	if (!args || !envp || !*envp)
+		return ;
+	while (*++args)
+	{
+		curr_node = *envp;
+		prev_node = NULL;
+		while (curr_node)
+		{
+			if (((t_envp *)curr_node->data)->key && \
+			!ft_strcmp(((t_envp *)curr_node->data)->key, *args))
+			{
+				if (prev_node)
+					prev_node->next = curr_node->next;
+				else
+					*envp = curr_node->next;
+				ft_lstdelone(curr_node, free_envp);
+				break ;
+			}
+			prev_node = curr_node;
+			curr_node = curr_node->next;
+		}
+	}
+}
