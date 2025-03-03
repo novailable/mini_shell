1b2b36a232935b29be05de0a7e3f3fe3d5bf9914
Nann-theDeveloper-001 <nangsuhtetsan94@gmail.com>
2025-03-03 20:11:48 +0800

    lexing_valgrind_done_nsan

--- minishell_2.0/Libft/ft_strcmp.c
+++ minishell_2.0/Libft/ft_strcmp.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -27,33 +26,3 @@ int	ft_strcmp(const char *s1, const char *s2)
 	}
 	return (*str1 - *str2);
 }
-=======
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: aoo <aoo@student.42singapore.sg>           +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/23 13:06:06 by aoo               #+#    #+#             */
-/*   Updated: 2025/02/24 10:59:22 by aoo              ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "libft.h"
-
-int	ft_strcmp(const char *s1, const char *s2)
-{
-	unsigned char	*str1;
-	unsigned char	*str2;
-
-	str1 = (unsigned char *)s1;
-	str2 = (unsigned char *)s2;
-	while (*str1 && *str2 && *str1 == *str2)
-	{
-		str1++;
-		str2++;
-	}
-	return (*str1 - *str2);
-}
->>>>>>> 4cdeb0f6219f9fcf8d9941a671c90a0876c7291f
--- minishell_2.0/built_in/echo.c
+++ minishell_2.0/built_in/echo.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -32,38 +31,3 @@ int	echo_n(char **args, t_list *envp, int status)
 		printf("\n");
 	return (status);
 }
-=======
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   echo.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: aoo <aoo@student.42singapore.sg>           +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/21 21:18:39 by aoo               #+#    #+#             */
-/*   Updated: 2025/02/24 11:32:41 by aoo              ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "../minishell.h"
-
-int	echo_n(char **args, t_list *envp, int status)
-{
-	int 	n_line;
-
-	n_line = 1;
-	if (args[1])
-		n_line = ft_strcmp("-n", args[1]);
-	if (n_line == 0)
-		args++;
-	while (*(++args))
-	{
-		printf("%s", *args);
-		if (*(args + 1))
-			printf(" ");
-	}
-	if (n_line != 0)
-		printf("\n");
-	return (status);
-}
->>>>>>> 4cdeb0f6219f9fcf8d9941a671c90a0876c7291f
