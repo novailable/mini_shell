6a0fe2ea828a34e765f348ca7b346d3f8f55070c
Nann-theDeveloper-001 <nangsuhtetsan94@gmail.com>
2025-03-03 20:11:48 +0800

    lexing_valgrind_done_nsan

--- minishell_2.0/ast_args.c
+++ minishell_2.0/ast_args.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -40,46 +39,3 @@ void	args_list_len_alloc(t_tokens *whole_list, char ***args_list)
 	}
 	*args_list = malloc(sizeof(char *) * (args_len + 1));
 }
-=======
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   args.c                                             :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: nsan <nsan@student.42.fr>                  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/01/14 07:18:09 by nsan              #+#    #+#             */
-/*   Updated: 2025/01/14 07:18:09 by nsan             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "minishell.h"
-
-void	args_list_len_alloc(t_tokens *whole_list, char ***args_list)
-{
-	t_tokens	*current;
-	int			args_len;
-
-	args_len = 0;
-	current = whole_list;
-	while (current && current->tok_types != T_PIPE)
-	{
-		if (current->tok_types == T_WORD)
-			args_len++;
-		else if (current->tok_types == T_REDIRECT_OUT || \
-		current->tok_types == T_REDIRECT_IN || \
-		current->tok_types == T_APPEND)
-		{
-			if (current->next->tok_types == T_WORD)
-				current = current->next;
-		}
-		current = current->next;
-	}
-	if (args_len == 0)
-	{
-		*args_list = NULL;
-		return ;
-	}
-	*args_list = malloc(sizeof(char *) * (args_len + 1));
-}
->>>>>>> 4cdeb0f6219f9fcf8d9941a671c90a0876c7291f
--- minishell_2.0/ast_branch.c
+++ minishell_2.0/ast_branch.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -22,28 +21,3 @@ t_tokens	*args_redirection_list(t_ast *left_node, t_tokens *whole_list)
 	temp = parse_cmd (left_node, whole_list);
 	return (temp);
 }
-=======
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ast_branch.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: nsan <nsan@student.42.fr>                  +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/11 19:22:55 by nsan              #+#    #+#             */
-/*   Updated: 2025/02/11 19:22:55 by nsan             ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include "minishell.h"
-
-t_tokens	*args_redirection_list(t_ast *left_node, t_tokens *whole_list)
-{
-	t_tokens	*temp;
-
-	args_list_len_alloc (whole_list, &(left_node->args));
-	redirect_list_len_alloc (whole_list, &(left_node->redirect));
-	temp = parse_cmd (left_node, whole_list);
-	return (temp);
-}
->>>>>>> 4cdeb0f6219f9fcf8d9941a671c90a0876c7291f
